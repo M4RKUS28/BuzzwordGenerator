aa27924daf87851b6cf69c17ebaa556650859b50
Your Name <you@example.com>
2024-12-01 01:39:34 +0100

    add a

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+//import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/BuzzwordGenerator/', // Replace {repository-name} with the name of your GitHub repository
+});
